c3eb9d761512aeacee1a42a1c453c9d1a0698728
“Anju” <anjupathak9810@gmail.com>
2025-09-22 18:59:35 +0000

    ci-cd trial

---
+++ index.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.get('/', (req, res) => {
+  res.send('Hello World! Yeh meri pehli CI/CD pipeline hai Railway par!');
+});
+
+app.listen(PORT, () => {
+  console.log(`Server ${PORT} par chal raha hai`);
+});
